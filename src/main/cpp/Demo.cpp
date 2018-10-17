8a450ed7a3431dba61af55e21e96e40b25092b42
Jim Daehn <jrd2112@missouristate.edu>
2018-10-17 17:15:01 -0500

    Initial commit of project targets.

---
+++ src/main/cpp/Demo.cpp
@@ -0,0 +1,21 @@
+/**
+ * CSC232 Data Structures with C++
+ * Missouri State University, Fall 2018.
+ *
+ * @file   Demo.cpp
+ * @author Jim Daehn <jdaehn@missouristate.edu>
+ * @brief  Entry point for gmsSoln target.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * Entry point for gmsSoln target.
+
+ * @return EXIT_SUCCESS is returned.
+ */
+int main() {
+	std::cout << "This is the gmsSolnDemo target." << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ src/main/cpp/Main.cpp
@@ -0,0 +1,21 @@
+/**
+ * CSC232 Data Structures with C++
+ * Missouri State University, Fall 2018.
+ *
+ * @file   Main.cpp
+ * @author Jim Daehn <jdaehn@missouristate.edu>
+ * @brief  Entry point for gmsSoln target.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/**
+ * Entry point for gmsSoln target.
+
+ * @return EXIT_SUCCESS is returned.
+ */
+int main() {
+	std::cout << "This is the gmsSoln target." << std::endl;
+	return EXIT_SUCCESS;
+}
